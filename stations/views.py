75bd82b0a8b07850ef2d4c9bf23c456f80598ed9
Leonid Taranov <supertaranov@gmail.com>
2023-03-31 21:55:53 +0700

    update

--- stations/views.py
+++ stations/views.py
@@ -9,15 +9,19 @@ def index(request):
 def bus_stations(request):
     with open('data-398-2018-08-30.csv', newline='', encoding="utf8") as csvfile:
         reader = csv.DictReader(csvfile)
+        stations = []
         for row in reader:
-            stations = (row['Name'], row['Street'], row['District'])
+            stations.append(
+                {'Name': row['Name'],
+                 'Street': row['Street'],
+                 'District': row['District']})
     # получите текущую страницу и передайте ее в контекст
     # также передайте в контекст список станций на странице
     page_number = int(request.GET.get('page', 1))
-    paginator = Paginator(stations, 10)
-    page = paginator.get_page(page_number)
+    pagi = Paginator(stations, 10)
+    page = pagi.get_page(page_number)
     context = {
-            'bus_stations': stations,
+            'bus_stations': page.object_list,
             'page': page,
     }    
     return render(request, 'stations/index.html', context)
